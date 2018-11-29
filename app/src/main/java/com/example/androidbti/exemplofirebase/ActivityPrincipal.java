97db993b0e5d703d5b6a42f54a59dda1595ef518
pedrohcavalcante <pedrohbcavalcante@ufrn.edu.br>
2018-11-29 18:17:23 -0300

    correcao

--- app/src/main/java/com/example/androidbti/exemplofirebase/ActivityPrincipal.java
+++ app/src/main/java/com/example/androidbti/exemplofirebase/ActivityPrincipal.java
@@ -2,12 +2,23 @@ package com.example.androidbti.exemplofirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ActivityPrincipal extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void sair(View view) {
+        firebaseAuth.signOut();
+        finish();
     }
 }
--- app/src/main/java/com/example/androidbti/exemplofirebase/MainActivity.java
+++ app/src/main/java/com/example/androidbti/exemplofirebase/MainActivity.java
@@ -5,16 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     public void login(View view) {
-        startActivity(new Intent(this, LoginActivity.class));
+        firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            startActivity(new Intent(this, LoginActivity.class));
+        } else {
+            startActivity(new Intent(this, ActivityPrincipal.class));
+
+        }
 
     }
 
